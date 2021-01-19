4fc15ab879e34b6996ec4ecc397080957abf4e04
Jackson Maxfield Brown <jmaxfieldbrown@gmail.com>
2021-01-18 19:14:13 -0800

    admin/fix-and-use-event-gather-pipeline (#14)
    * Make the python dir a pip installable mod
    
    * Do not run infra deployment on schedule
    
    * Add python_municipality_slug for reuse
    
    * Store cookiecutter options in the repo
    
    * Fix typo in maintainer_full_name
    
    * Fix tests to actually check the python module
    
    * Use example as name instead of cookiecutter fill

---
+++ {{ cookiecutter.municipality_slug }}/python/cdp_{{ cookiecutter.python_municipality_slug }}_backend/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Top-level package for {{ cookiecutter.municipality }}
+CDP instance backend.
+"""
+
+__author__ = "{{ cookiecutter.maintainer_full_name }}"
+__version__ = "1.0.0"
+
+
+def get_module_version() -> str:
+    return __version__
---
+++ {{ cookiecutter.municipality_slug }}/python/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+test_requirements = [
+    "black>=19.10b0",
+    "flake8>=3.8.3",
+    "flake8-debugger>=3.2.1",
+]
+
+dev_requirements = [
+    *test_requirements,
+    "wheel>=0.34.2",
+]
+
+requirements = [
+    "cdp-backend~=3.0.0.dev5",
+]
+
+extra_requirements = {
+    "test": test_requirements,
+    "dev": dev_requirements,
+    "all": [
+        *requirements,
+        *dev_requirements,
+    ],
+}
+
+setup(
+    author="{{ cookiecutter.maintainer_full_name }}",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.9",
+    ],
+    description="Package containing the gather functions for Example.",
+    install_requires=requirements,
+    license="MIT license",
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords="civic technology, open government",
+    name="cdp-{{ cookiecutter.python_municipality_slug }}-backend",
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*"]),
+    python_requires=">=3.9",
+    tests_require=test_requirements,
+    extras_require=extra_requirements,
+    url="https://github.com/{{ cookiecutter.hosting_github_username_or_org }}/{{ cookiecutter.municipality_slug }}",
+    version="1.0.0",
+    zip_safe=False,
+)
