1ef114d1c8614aac8004eebda270caee85f0135d
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-01-19 03:17:37 +0000

    Deploying to example-build from main @ 4fc15ab879e34b6996ec4ecc397080957abf4e04 🚀

---
+++ python/cdp_example_backend/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Top-level package for Example
+CDP instance backend.
+"""
+
+__author__ = "Jackson Maxfield Brown"
+__version__ = "1.0.0"
+
+
+def get_module_version() -> str:
+    return __version__
