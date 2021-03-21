b38e030045c9273e7dce67afb0f7c07870b6efce
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-03-21 16:46:26 -0700

    Try using pre and post gen hooks

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print("HELLO!")
+
+# import json
+
+# with open("cookiecutter.json", "r") as open_f:
+#     params = json.load(open_f)
+
+# print(params)
