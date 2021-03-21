a9b5c532e04514ad72c139688c3cb8d78316356a
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-03-21 16:56:38 -0700

    Remove quotes around filename

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,5 +10,5 @@ print(f"Created new directory: {deployment_dir}")
 print(
     "To finish new CDP Instance creation, move into the "
     "created directory and follow the instructions in the "
-    "'Repo Setup' section of the 'README.md' file."
+    "'Repo Setup' section of the README.md file."
 )
