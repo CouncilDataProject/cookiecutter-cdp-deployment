ce9ad40bfd27d5da41d979a4026967016b7f64ed
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-03-21 16:54:44 -0700

    Add intermediate instructions in post hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,1 +1,14 @@
-print("END")
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+deployment_dir = Path("{{ cookiecutter.municipality_slug }}").resolve()
+
+print("-" * 80)
+print(f"Created new directory: {deployment_dir}")
+print(
+    "To finish new CDP Instance creation, move into the "
+    "created directory and follow the instructions in the "
+    "'Repo Setup' section of the 'README.md' file."
+)
