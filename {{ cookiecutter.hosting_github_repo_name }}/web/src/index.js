9b11452899723347863b0a12b06cee3dc422003a
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-05-29 11:24:33 -0700

    Add a temporary App component to src/index.js until CDP App is finished

--- {{ cookiecutter.hosting_github_repo_name }}/web/src/index.js
+++ {{ cookiecutter.hosting_github_repo_name }}/web/src/index.js
@@ -1,4 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from '@councildataproject/cdp-frontend';
+// import {App} from '@councildataproject/cdp-frontend';
+
+// Temporary App component until the CDP App component is 
+// finished and published
+function App() {
+  return <h1>CDP App Instance</h1>
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
