3342b4ffa362feb362c6f2af98e1c3e41211bd60
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-06-03 22:20:12 +0000

    Deploying to example-build from main @ 7bb92b211bac7a0d2b85434a8dffb23069cbfbfb 🚀

--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import {App} from '@councildataproject/cdp-frontend';
+
+// Temporary App component until the CDP App component is 
+// finished and published
+function App() {
+  return <h1>CDP App Instance</h1>
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
