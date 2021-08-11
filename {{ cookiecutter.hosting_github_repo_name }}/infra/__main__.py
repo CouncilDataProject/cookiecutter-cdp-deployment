65520260f5c8cb3d27cb50c76183546bcf2f9c09
isaac219 <isaac219@gmail.com>
2021-08-11 00:49:30 -0700

    Add firestore region list option

--- {{ cookiecutter.hosting_github_repo_name }}/infra/__main__.py
+++ {{ cookiecutter.hosting_github_repo_name }}/infra/__main__.py
@@ -6,7 +6,7 @@ from pulumi import export
 
 ###############################################################################
 
-cdp_stack = CDPStack("cdp-{{ cookiecutter.municipality_slug }}")
+cdp_stack = CDPStack("cdp-{{ cookiecutter.municipality_slug }}", "{{ cookiecutter.firestore_region }}")
 
 export("firestore_address", cdp_stack.firestore_app.app_id)
 export("gcp_bucket_name", cdp_stack.firestore_app.default_bucket)
