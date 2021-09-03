86d78272a46461b22857056604bb4fd54bf1d3bd
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-09-03 09:36:23 -0700

    Split up parsing form into it's own script

--- .github/workflows/scripts/validate_form.py
+++ .github/workflows/scripts/validate_form.py
@@ -7,12 +7,23 @@ import json
 import logging
 import sys
 import traceback
-from typing import Dict, List, Optional
 
-from cdp_backend.utils.string_utils import clean_text
 from cdp_scrapers.legistar_utils import LegistarScraper
 from cdp_scrapers import instances
 import requests
+
+from parse_form import (
+    COOKIECUTTER_OPTIONS,
+    FORM_VALUES,
+    MUNICIPALITY_SLUG,
+    MUNICIPALITY_NAME,
+    LEGISTAR_CLIENT_ID,
+    LEGISTAR_CLIENT_TIMEZONE,
+    PYTHON_MUNICIPALITY_SLUG,
+    TARGET_MAINTAINER,
+    COUNCIL_DATA_PROJECT,
+    parse_form,
+)
 
 ###############################################################################
 
@@ -24,32 +35,8 @@ log = logging.getLogger(__name__)
 
 ###############################################################################
 
-COUNCIL_DATA_PROJECT = "CouncilDataProject"
-
-MUNICIPALITY_NAME = "municipality"
-MUNICIPALITY_SLUG = "municipality_slug"
-PYTHON_MUNICIPALITY_SLUG = "python_municipality_slug"
-TARGET_MAINTAINER = "maintainer_or_org_full_name"
-FIRESTORE_REGION = "firestore_region"
-
-LEGISTAR_CLIENT_ID = "legistar_client_id"
-LEGISTAR_CLIENT_TIMEZONE = "legistar_client_timezone"
-
-FORM_FIELD_TO_HEADER = {
-    MUNICIPALITY_NAME: "Municipality Name",
-    MUNICIPALITY_SLUG: "Municipality Slug",
-    TARGET_MAINTAINER: "Maintainer GitHub Name",
-    FIRESTORE_REGION: "Firestore Region",
-    LEGISTAR_CLIENT_ID: "Legistar Client Id",
-    LEGISTAR_CLIENT_TIMEZONE: "Municipality Timezone",
-}
-
 GITHUB_USERS_RESOURCE = "users"
 GITHUB_REPOSITORIES_RESOURCE = "repos"
-
-NO_RESPONSE = "_No response_"
-
-DEFAULT_FIRESTORE_REGION = "us-central1"
 
 ###############################################################################
 
@@ -73,13 +60,6 @@ class Args(argparse.Namespace):
         p.parse_args(namespace=self)
 
 
-def _get_field_value(lines: List[str], field_header: str) -> Optional[str]:
-    # Get index of target then + 1 for the value
-    header_index = lines.index(f"### {field_header}")
-    value = lines[header_index + 1]
-    return value if value != NO_RESPONSE else None
-
-
 def _check_github_resource_exists(resource: str, name: str) -> bool:
     # Request and get response
     response = requests.get(f"https://api.github.com/{resource}/{name}")
@@ -93,36 +73,21 @@ def _check_github_resource_exists(resource: str, name: str) -> bool:
 
 
 def validate_form(issue_content_file: str) -> None:
-    # Open the content file, read, strip, and clean
-    with open(issue_content_file, "r") as open_f:
-        lines = open_f.readlines()
-        lines = [line.strip() for line in lines]
-        lines = [line for line in lines if len(line) > 0]
+    # Parse and get cookiecutter options
+    form_values_and_cookiecutter_options = parse_form(
+        issue_content_file=issue_content_file,
+    )
 
-    # Get all form values
-    form_values: Dict[str, Optional[str]] = {}
-    for field_name, form_header_string in FORM_FIELD_TO_HEADER.items():
-        form_values[field_name] = _get_field_value(
-            lines=lines,
-            field_header=form_header_string,
-        )
-    log.info(form_values)
+    # Unpack form values
+    form_values = form_values_and_cookiecutter_options[FORM_VALUES]
 
-    # Get municipality slug
-    if form_values[MUNICIPALITY_SLUG] is None:
-        municipality_slug = (
-            clean_text(form_values[MUNICIPALITY_NAME])
-            .lower()
-            .replace(
-                " ",
-                "-",
-            )
-        )
-    else:
-        municipality_slug = form_values[MUNICIPALITY_SLUG]
-
-    # Get python municipality slug
-    python_municipality_slug = municipality_slug.replace("-", "_")
+    # Unpack certain values
+    municipality_slug = form_values_and_cookiecutter_options[COOKIECUTTER_OPTIONS][
+        MUNICIPALITY_SLUG
+    ]
+    python_municipality_slug = form_values_and_cookiecutter_options[
+        COOKIECUTTER_OPTIONS
+    ][PYTHON_MUNICIPALITY_SLUG]
 
     # Check planned maintainer exists
     planned_maintainer_exists = _check_github_resource_exists(
@@ -135,36 +100,6 @@ def validate_form(issue_content_file: str) -> None:
     planned_repository_exists = _check_github_resource_exists(
         resource=GITHUB_REPOSITORIES_RESOURCE, name=repository_path
     )
-
-    # Get default firestore region
-    if form_values[FIRESTORE_REGION] is None:
-        firestore_region = DEFAULT_FIRESTORE_REGION
-    else:
-        firestore_region = form_values[FIRESTORE_REGION]
-
-    # Dump to cookiecutter.json
-    with open("planned-cookiecutter.json", "w") as open_f:
-        open_f.write(
-            json.dumps(
-                {
-                    MUNICIPALITY_NAME: form_values[MUNICIPALITY_NAME],
-                    MUNICIPALITY_SLUG: municipality_slug,
-                    PYTHON_MUNICIPALITY_SLUG: python_municipality_slug,
-                    TARGET_MAINTAINER: form_values[TARGET_MAINTAINER],
-                    "hosting_github_username_or_org": COUNCIL_DATA_PROJECT,
-                    "hosting_github_repo_name": municipality_slug,
-                    "hosting_github_url": (
-                        f"https://github.com/"
-                        f"{COUNCIL_DATA_PROJECT}/{municipality_slug}"
-                    ),
-                    "hosting_web_app_address": (
-                        f"https://councildataproject.org/{municipality_slug}"
-                    ),
-                    FIRESTORE_REGION: firestore_region,
-                },
-                indent=4,
-            )
-        )
 
     # Test Legistar / existing scraper
     scraper_response = None
