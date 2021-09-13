5c6166746d0d7a88b15466efc772001cdda65e2d
JacksonMaxfield <JacksonMaxfield@users.noreply.github.com>
2021-09-13 23:36:45 +0000

    Deploying to example-build from main @ 722f5bc2ea46e1394ce83ecd1fd9433c3c8601aa 🚀

--- infra/__main__.py
+++ infra/__main__.py
@@ -7,7 +7,7 @@ from pulumi import export
 ###############################################################################
 
 cdp_stack = CDPStack(
-    gcp_project_id="cdp-example-xhHxAUvR",
+    gcp_project_id="cdp-example-IIxkylGT",
     firestore_location="us-central",
 )
 
