c6937c5cf37d8b1dbe785b68e9233b3cd61949b2
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-09-26 21:30:35 -0700

    Update to get it partially working

--- {{ cookiecutter.hosting_github_repo_name }}/web/src/index.jsx
+++ {{ cookiecutter.hosting_github_repo_name }}/web/src/index.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App, AppConfigProvider } from "@councildataproject/cdp-frontend";
 
+import "@councildataproject/cdp-design/dist/images.css";
+import "@councildataproject/cdp-design/dist/colors.css";
+import "@mozilla-protocol/core/protocol/css/protocol.css";
+import "@mozilla-protocol/core/protocol/css/protocol-components.css";
+import "semantic-ui-css/semantic.min.css";
+
 const config = {
     firebaseConfig: {
         options: {
