ec9eb013ac61e7ddb6b06bdb1fa0fc21d4f4b75b
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-10-06 22:45:40 -0700

    Use react-scripts for everything

--- {{ cookiecutter.hosting_github_repo_name }}/web/src/index.jsx
+++ {{ cookiecutter.hosting_github_repo_name }}/web/src/index.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App, AppConfigProvider } from "@councildataproject/cdp-frontend";
 
-import "@councildataproject/cdp-design/dist/images.css";
-import "@councildataproject/cdp-design/dist/colors.css";
-import "@mozilla-protocol/core/protocol/css/protocol.css";
-import "@mozilla-protocol/core/protocol/css/protocol-components.css";
-import "semantic-ui-css/semantic.min.css";
+import "@councildataproject/cdp-frontend/dist/index.css";
 
 const config = {
     firebaseConfig: {
