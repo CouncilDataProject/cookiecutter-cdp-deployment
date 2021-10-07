8e624aa2f634307f2d0b9318e58ae3125b97e317
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-10-07 05:52:49 +0000

    Deploying to example-build from main @ 714ba10a9580a9d7c233fefc836d40097b7952f7 🚀

--- infra/__main__.py
+++ infra/__main__.py
@@ -7,7 +7,7 @@ from pulumi import export
 ###############################################################################
 
 cdp_stack = CDPStack(
-    gcp_project_id="cdp-example-kihjpgjw",
+    gcp_project_id="cdp-example-aimooarc",
     firestore_location="us-central",
 )
 
---
+++ web/src/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App, AppConfigProvider } from "@councildataproject/cdp-frontend";
+
+import "@councildataproject/cdp-frontend/dist/index.css";
+
+const config = {
+    firebaseConfig: {
+        options: {
+            projectId: "cdp-example-aimooarc",
+        },
+        settings: {},
+    },
+    municipality: {
+        name: "Example",
+        footerLinksSections: [],
+    },
+}
+
+ReactDOM.render(
+    <div>
+        <AppConfigProvider appConfig={config}>
+            <App />
+        </AppConfigProvider>
+    </div>,
+    document.getElementById("root")
+);
