714ba10a9580a9d7c233fefc836d40097b7952f7
Jackson Maxfield Brown <jmaxfieldbrown@gmail.com>
2021-10-06 22:49:23 -0700

    feature/import-web-app-and-use-in-build (#56)
    * Update backend pin
    
    * Attempt to get app into cookiecutter
    
    * Wrap config in raw strings
    
    * Fix cookiecutter var name
    
    * Remove lint from web tests in PR
    
    * Rename file from index.js to index.jsx
    
    * Pull out config from render
    
    * Update to get it partially working
    
    * Bump to cdp-frontend 0.4.2
    
    * Use react-scripts for everything

---
+++ {{ cookiecutter.hosting_github_repo_name }}/web/src/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App, AppConfigProvider } from "@councildataproject/cdp-frontend";
+
+import "@councildataproject/cdp-frontend/dist/index.css";
+
+const config = {
+    firebaseConfig: {
+        options: {
+            projectId: "{{ cookiecutter.infrastructure_slug }}",
+        },
+        settings: {},
+    },
+    municipality: {
+        name: "{{ cookiecutter.municipality }}",
+        footerLinksSections: [],
+    },
+}
+
+ReactDOM.render(
+    <div>
+        <AppConfigProvider appConfig={config}>
+            <App />
+        </AppConfigProvider>
+    </div>,
+    document.getElementById("root")
+);
