f1d5153e7c62bc949578c8561d4210ec99ec9d31
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-11-04 02:42:08 +0000

    Deploying to example-build from main @ 965404183e60dcaf36b4ef082fcbfeac839fa244 🚀

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -7,7 +7,7 @@ import "@councildataproject/cdp-frontend/dist/index.css";
 const config = {
     firebaseConfig: {
         options: {
-            projectId: "cdp-example-uclqfjep",
+            projectId: "cdp-example-xhxljvjs",
         },
         settings: {},
     },
