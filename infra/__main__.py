0ef5d9c69dfe4838b3b81f7e6ca719a11a585ca7
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-11-09 04:56:39 +0000

    Deploying to example-build from main @ ebbf0c573ec93be5e00dfef207a80f41b968aefb 🚀

--- infra/__main__.py
+++ infra/__main__.py
@@ -7,7 +7,7 @@ from pulumi import export
 ###############################################################################
 
 cdp_stack = CDPStack(
-    gcp_project_id="cdp-example-fsijkyuz",
+    gcp_project_id="cdp-example-usptvjgo",
     firestore_location="us-central",
 )
 
--- web/src/index.jsx
+++ web/src/index.jsx
@@ -7,7 +7,7 @@ import "@councildataproject/cdp-frontend/dist/index.css";
 const config = {
     firebaseConfig: {
         options: {
-            projectId: "cdp-example-fsijkyuz",
+            projectId: "cdp-example-usptvjgo",
         },
         settings: {},
     },
