ee7b7a7fb9173085949aa0f952e449aa5d40607f
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2022-02-14 01:59:35 +0000

    Deploying to example-build from main @ cc19215ae4426c5cb8d4b3b7a7f5e345bd964e88 🚀

--- infra/__main__.py
+++ infra/__main__.py
@@ -7,7 +7,7 @@ from pulumi import export
 ###############################################################################
 
 cdp_stack = CDPStack(
-    gcp_project_id="cdp-example-ehnlbbwq",
+    gcp_project_id="cdp-example-xsvqtdop",
     municipality_name="Example",
     firestore_location="us-central",
     hosting_github_url="https://github.com/CouncilDataProject/example",
