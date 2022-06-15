5483e7d05b33c0d48e3d6efb4cff91b669f9e70a
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2022-06-15 22:16:00 +0000

    Deploying to example-build from main @ f26e4bbc6f52adaca3e3b3a5af486916b9b634a7 🚀

--- python/setup.py
+++ python/setup.py
@@ -17,7 +17,7 @@ dev_requirements = [
 ]
 
 requirements = [
-    "cdp-backend[pipeline]==3.0.16",
+    "cdp-backend[pipeline]==3.1.1",
 ]
 
 extra_requirements = {
--- web/src/index.jsx
+++ web/src/index.jsx
@@ -7,7 +7,7 @@ import "@councildataproject/cdp-frontend/dist/index.css";
 const config = {
     firebaseConfig: {
         options: {
-            projectId: "cdp-example-qkyiytmw",
+            projectId: "cdp-example-imhxbksl",
         },
         settings: {},
     },
