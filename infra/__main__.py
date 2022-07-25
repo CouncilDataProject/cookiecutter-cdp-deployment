8f37fcffc46c9bb6af9b127fddd29b0e5e020791
JacksonMaxfield <JacksonMaxfield@users.noreply.github.com>
2022-07-25 23:40:43 +0000

    Deploying to example-build from main @ 75ba2c77c3cdba4aeac185e0d724ed12ff5bd62c 🚀

--- infra/__main__.py
+++ infra/__main__.py
@@ -7,7 +7,7 @@ from pulumi import export
 ###############################################################################
 
 cdp_stack = CDPStack(
-    gcp_project_id="cdp-example-yxyzpgnb",
+    gcp_project_id="cdp-example-umcgyijo",
     municipality_name="Example",
     firestore_location="us-central",
     hosting_github_url="https://github.com/CouncilDataProject/example",
--- web/src/index.jsx
+++ web/src/index.jsx
@@ -7,7 +7,7 @@ import "@councildataproject/cdp-frontend/dist/index.css";
 const config = {
     firebaseConfig: {
         options: {
-            projectId: "cdp-example-yxyzpgnb",
+            projectId: "cdp-example-umcgyijo",
         },
         settings: {},
     },
