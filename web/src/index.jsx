e0e953dc2af26fe5f13553128289a57942835887
evamaxfield <evamaxfieldbrown@gmail.com>
2022-09-11 03:05:25 +0000

    Deploying to gh-pages from @ CouncilDataProject/cookiecutter-cdp-deployment@b51684cab7342198225ac923683384f4d5f8f757 🚀

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -7,7 +7,7 @@ import "@councildataproject/cdp-frontend/dist/index.css";
 const config = {
     firebaseConfig: {
         options: {
-            projectId: "cdp-example-mqyyxjmy",
+            projectId: "cdp-example-ghjiyivr",
         },
         settings: {},
     },
