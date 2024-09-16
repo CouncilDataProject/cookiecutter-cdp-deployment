ad2b35864a4729b55ed2761a507670d98f3c35fc
JacksonMaxfield <JacksonMaxfield@users.noreply.github.com>
2024-09-16 23:36:13 +0000

    Deploying to gh-pages from @ CouncilDataProject/cookiecutter-cdp-deployment@84c13f27ea569441399b6b596878eb3fb29845d3 🚀

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -7,7 +7,7 @@ import "@councildataproject/cdp-frontend/dist/index.css";
 const config = {
     firebaseConfig: {
         options: {
-            projectId: "cdp-example-npbsswwr",
+            projectId: "cdp-example-vnorvkzf",
         },
         settings: {},
     },
